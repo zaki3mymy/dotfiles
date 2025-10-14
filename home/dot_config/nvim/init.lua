64796151065d98fe8812287843e514c95f341cbb
zaki3mymy <zaki.last@gmail.com>
2025-10-14 23:45:46 +0900

    lazy.nvimの設定を追記

--- home/dot_config/nvim/init.lua
+++ home/dot_config/nvim/init.lua
@@ -12,3 +12,20 @@ vim.api.nvim_set_keymap('i', 'jj', '<ESC>', {noremap = true, silent = true})
 -- クリップボードの使用
 vim.opt.clipboard:append({"unnamedplus"})
 
+-- プラグインマネージャー(lazy.nvim)のインストール
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable",
+        lazypath,
+    })
+end
+vim.opt.rtp:prepend(lazypath)
+local plugins = {}
+local opts = {}
+require("lazy").setup(plugins, opts)
+
