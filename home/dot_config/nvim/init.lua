30c052663677a91aa03ad5068a1da0da1b7cf2c5
zaki3mymy <zaki.last@gmail.com>
2025-10-18 12:25:08 +0900

    前回編集時のカーソル位置で開く

--- home/dot_config/nvim/init.lua
+++ home/dot_config/nvim/init.lua
@@ -125,3 +125,15 @@ vim.cmd("set whichwrap=<,>,h,l")
 -- 上端と下端に余裕をもたせる
 vim.cmd("set scrolloff=5")
 
+-- 前回終了時の位置にカーソルを移動する
+vim.api.nvim_create_autocmd("BufReadPost", {
+    pattern = "*",
+    callback = function()
+        local mark = vim.api.nvim_buf_get_mark(0, '"')
+        local lcount = vim.api.nvim_buf_line_count(0)
+        if mark[1] > 0 and mark[1] <= lcount then
+            pcall(vim.api.nvim_win_set_cursor, 0, mark)
+        end
+    end,
+})
+
