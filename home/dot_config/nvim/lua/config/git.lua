032fcf12797e6b9f9012772ce6df3c5eeaa8f394
zaki3mymy <zaki.last@gmail.com>
2025-12-01 21:10:54 +0900

    ハンクの移動のショートカットを追加
    ]c / [c でそれぞれ次のハンク、前のハンク

--- home/dot_config/nvim/lua/config/git.lua
+++ home/dot_config/nvim/lua/config/git.lua
@@ -4,6 +4,31 @@ require("gitsigns").setup({
     current_line_blame = true,
     current_line_blame_opts = {
         delay = 100
-    }
+    },
+    on_attach = function(bufnr)
+        local gitsigns = require("gitsigns")
+
+        local function map(mode, l, r, opts)
+            opts = opts or {}
+            opts.buffer = bufnr
+            vim.keymap.set(mode, l, r, opts)
+        end
+
+        -- Navigation
+        map("n", "]c", function()
+            if vim.wo.diff then
+                vim.cmd.normal({"]c", bang = true})
+            else
+                gitsigns.nav_hunk("next")
+            end
+        end)
+        map("n", "[c", function()
+            if vim.wo.diff then
+                vim.cmd.normal({"[c", bang = true})
+            else
+                gitsigns.nav_hunk("prev")
+            end
+        end)
+    end
 })
 
