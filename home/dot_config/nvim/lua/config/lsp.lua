2a15810a7a8a98ac4e6191db9eda21ce9a11a4ff
zaki3mymy <zaki.last@gmail.com>
2025-12-11 23:51:58 +0900

    LSPのキーマップの設定

--- home/dot_config/nvim/lua/config/lsp.lua
+++ home/dot_config/nvim/lua/config/lsp.lua
@@ -46,3 +46,17 @@ vim.lsp.config("lua_ls", {
 })
 vim.lsp.enable(ensure_installed)
 
+-- キーマップの設定
+-- カーソル下の変数の情報
+vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>")
+-- 定義ジャンプ
+vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>")
+-- 定義ジャンプ後に下のファイルに戻る
+vim.keymap.set("n", "gt", "<C-t>")
+-- 改行やインデントなどのフォーマット
+vim.keymap.set("n", "gf", "<cmd>lua vim.lsp.buf.formatting()<CR>")
+-- カーソル下の変数をコード内で参照している箇所
+vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>")
+-- 変数名のリネーム
+vim.keymap.set("n", "gn", "<cmd>lua vim.lsp.buf.rename()<CR>")
+
