b930548e680e3c98b0510a92fdf26526fe47a5a8
zaki3mymy <zaki.last@gmail.com>
2025-12-11 22:22:48 +0900

    タブ操作のキーマップを<leader>起点に統一

--- home/dot_config/nvim/lua/config/tabs.lua
+++ home/dot_config/nvim/lua/config/tabs.lua
@@ -3,7 +3,7 @@ require("bufferline").setup({
     options = {
     },
 })
-vim.keymap.set("n", "<C-l>", "<Cmd>bnext!<CR>")
-vim.keymap.set("n", "<C-h>", "<Cmd>bprev!<CR>")
---vim.keymap.set("n", "<leader><C-w>", "<Cmd>BufferLinePickClose<CR>")
+vim.keymap.set("n", "<leader>l", "<Cmd>bnext!<CR>")
+vim.keymap.set("n", "<leader>h", "<Cmd>bprev!<CR>")
+vim.keymap.set("n", "<leader>w", "<Cmd>bd<CR>")
 
