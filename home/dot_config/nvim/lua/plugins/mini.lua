9ecf14baf71d1e3cb06ee3424c19fb6ac86b054e
zaki3mymy <zaki.last@gmail.com>
2025-12-13 14:58:17 +0900

    mini.nvimのプラグインは一つのファイルにまとめる

---
+++ home/dot_config/nvim/lua/plugins/mini.lua
@@ -0,0 +1,11 @@
+return {
+    {
+        "nvim-mini/mini.pairs",
+        version = false,
+    },
+    {
+        "nvim-mini/mini.nvim",
+        version = false,
+    },
+}
+
--- home/dot_config/nvim/lua/plugins/mini.pairs.lua
+++
@@ -1,5 +0,0 @@
-return {
-    "nvim-mini/mini.pairs",
-    version = false,
-}
-
