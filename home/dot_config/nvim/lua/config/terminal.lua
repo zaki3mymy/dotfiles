07856823cde52743d1dbcca6673255fd9f831128
zaki3mymy <zaki.last@gmail.com>
2025-12-13 17:42:47 +0900

    `<leader>@`でターミナルをホバー表示

---
+++ home/dot_config/nvim/lua/config/terminal.lua
@@ -0,0 +1,9 @@
+require("toggleterm").setup({
+    open_mapping = [[<leader>@]],
+    direction = "float",
+})
+
+-- フロートで表示するので以下のマッピングは不要
+-- -- ターミナルから移動するための設定
+-- vim.keymap.set("t", "<C-w>", "<C-\\><C-n><C-w>")
+
