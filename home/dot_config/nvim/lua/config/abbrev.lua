dccc8ab78d04ffde79dc6487ef7f8bb362f9be99
zaki3mymy <zaki.last@gmail.com>
2025-12-25 23:10:18 +0900

    置換の短縮入力を`:s `でできるようにした

---
+++ home/dot_config/nvim/lua/config/abbrev.lua
@@ -0,0 +1,5 @@
+-- 置換の短縮入力
+-- https://zenn.dev/vim_jp/articles/2023-06-30-vim-substitute-tips
+vim.cmd(
+    [[cnoreabbrev <expr> s getcmdtype() .. getcmdline() ==# ':s' ? [getchar(), ''][1] .. "%s///g<Left><Left>" : 's']]
+)
