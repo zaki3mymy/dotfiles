e18b359dc60d33ea82d4151b60017fd2b9e13cad
zaki3mymy <zaki.last@gmail.com>
2025-12-26 21:34:09 +0900

    ターミナルの設定をplugins側に寄せた

--- home/dot_config/nvim/init.lua
+++ home/dot_config/nvim/init.lua
@@ -2,5 +2,4 @@ require("base")
 require("init_lazy")
 
 require("config.color-schema")
-require("config.terminal")
 require("config.lsp")
--- home/dot_config/nvim/lua/plugins/toggleterm.lua
+++ home/dot_config/nvim/lua/plugins/toggleterm.lua
@@ -2,6 +2,17 @@ return {
   {
     "akinsho/toggleterm.nvim",
     version = "*",
-    config = true,
+    config = function()
+      -- ターミナルモードから抜けるための設定
+      -- TODO: keymaps.luaに移動
+      vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], { noremap = true })
+      vim.keymap.set("t", "jj", [[<C-\><C-n>]], { noremap = true })
+
+      require("toggleterm").setup({
+        -- 同じキーを入力するとターミナルを閉じることができる
+        open_mapping = [[<leader>@]],
+        direction = "float",
+      })
+    end,
   },
 }
