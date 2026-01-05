9d009578548e910ac7a4402f66b6ca543ed94dd2
z3y <91773180+zaki3mymy@users.noreply.github.com>
2026-01-05 15:22:06 +0900

    挿入モードのときに関数のシグネチャを表示するショートカットを追加 (#44)

--- home/dot_config/nvim/lua/config/keymaps.lua
+++ home/dot_config/nvim/lua/config/keymaps.lua
@@ -29,6 +29,7 @@ vim.keymap.set("n", "<Esc><Esc>", "<Cmd>noh<CR>", { noremap = true, desc = "No s
 vim.keymap.set("n", "K", vim.lsp.buf.hover, { noremap = true, desc = "Hover info" })
 vim.keymap.set("n", "gr", vim.lsp.buf.references, { noremap = true, desc = "Show references" })
 vim.keymap.set("n", "ge", vim.diagnostic.open_float, { noremap = true, desc = "Show Error" })
+vim.keymap.set("i", "<leader>s", vim.lsp.buf.signature_help, { noremap = true, desc = "Show Signature" })
 -- ジャンプ
 vim.keymap.set("n", "gd", vim.lsp.buf.definition, { noremap = true, desc = "Go to definition" })
 vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { noremap = true, desc = "Go to definition" })
