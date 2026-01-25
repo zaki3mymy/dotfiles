a7fdd913d87d8e01f9e27bdca620cc950a3379ce
zaki3mymy <zaki.last@gmail.com>
2026-01-25 00:11:20 +0000

    oilで特定の隠しファイルは表示するようにする

--- home/dot_config/nvim/lua/plugins/oil.lua
+++ home/dot_config/nvim/lua/plugins/oil.lua
@@ -1,13 +1,35 @@
+-- 常に表示したい隠しファイル
+-- luaではエスケープは`\`ではなく`%`
+local always_show_files = {
+  "%.claude",
+  "%.github",
+  "%.gitconfig",
+  "%.gitignore",
+  "%..+%.local%..+", -- globalの.gitignoreで除外しているファイル
+}
+
 return {
   "stevearc/oil.nvim",
   ---@module 'oil'
   ---@type oil.SetupOpts
-  opts = {},
   dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
   -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
   lazy = false,
   config = function()
-    require("oil").setup()
+    require("oil").setup({
+      view_options = {
+        show_hidden = false,
+        is_hidden_file = function(name, _)
+          for _, regex in ipairs(always_show_files) do
+            if name:match(regex) then
+              return false
+            end
+          end
+          local m = name:match("^%.")
+          return m ~= nil
+        end,
+      },
+    })
   end,
   keys = {
     { "<leader>-", "<Cmd>Oil<CR>", mode = "n" },
