2c91bad8c90db55832fa02312d382fd4c8b85d90
zaki3mymy <zaki.last@gmail.com>
2026-04-25 07:51:34 +0000

    nix関連のファイルもdotfilesで扱う

---
+++ home/dot_config/home-manager/flake.nix
@@ -0,0 +1,18 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs"; # nixpkgsのバージョンを統一
+    };
+  };
+
+  outputs =
+    { nixpkgs, home-manager, ... }:
+    {
+      homeConfigurations."root" = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages."x86_64-linux";
+        modules = [ ./home.nix ];
+      };
+    };
+}
---
+++ home/dot_config/home-manager/home.nix
@@ -0,0 +1,77 @@
+{ pkgs, config, ... }:
+{
+  home.username = "root";
+  home.homeDirectory = "/root";
+  home.stateVersion = "25.11";
+
+  # zshを有効化
+  programs.zsh = {
+    enable = true;
+    dotDir = "${config.home.homeDirectory}/.config/zsh"; # chezmoiの設定を活かすためにnix管理の.zshrcはこっちで管理する
+
+    zsh-abbr = {
+      enable = true;
+    };
+
+    envExtra = ''
+      # chezmoiの.zshrcを活かすためにこの設定が必要
+      export ZDOTDIR="$HOME"
+    '';
+  };
+
+  # デフォルトシェルをzshに
+  home.sessionVariables = {
+    SHELL = "${pkgs.zsh}/bin/zsh";
+  };
+
+  # zsh-abbr は非商用利用のみ(cc-by-nc-sa-40 hl3)なので特別に許可する
+  nixpkgs.config.allowUnfreePredicate =
+    pkg:
+    builtins.elem (pkgs.lib.getName pkg) [
+      "zsh-abbr"
+      "claude-code"
+    ];
+
+  home.packages = with pkgs; [
+    actionlint
+    curl
+    git
+    git-secrets
+    gh
+    neovim
+    pre-commit
+    ripgrep
+    tmux
+    unzip
+    vim
+    zsh
+    zsh-abbr
+    zip
+
+    # Nix
+    home-manager
+    nixfmt
+
+    # dotfiles
+    chezmoi
+    stylua
+    shellcheck
+    shfmt
+
+    # Python
+    uv
+
+    # Go
+    go
+    golangci-lint
+
+    # IaC
+    tenv
+
+    # JavaScript / TypeScript
+    pnpm
+
+    # AI
+    claude-code
+  ];
+}
