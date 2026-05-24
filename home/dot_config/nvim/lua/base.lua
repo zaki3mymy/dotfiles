f389cb2493ddf0e26bb1df23b23672e3db77e706
zaki3mymy <zaki.last@gmail.com>
2026-05-24 11:10:29 +0900

    WSLのNixOSでクリップボードを使う設定

--- home/dot_config/nvim/lua/base.lua
+++ home/dot_config/nvim/lua/base.lua
@@ -20,3 +20,20 @@ vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter" }, {
   command = "if mode() != 'c' | checktime | endif",
   pattern = "*",
 })
+
+-- WSL クリップボード設定
+-- https://zenn.dev/link/comments/95d56a62268c6f
+if vim.fn.has "wsl" == 1 then
+  vim.g.clipboard = {
+    name = "win32yank",
+    copy = {
+      ["+"] = "win32yank.exe -i --crlf",
+      ["*"] = "win32yank.exe -i --crlf",
+    },
+    paste = {
+      ["+"] = "win32yank.exe -o --lf",
+      ["*"] = "win32yank.exe -o --lf",
+    },
+    cache_enabled = 0,
+  }
+end
