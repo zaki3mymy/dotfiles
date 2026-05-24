0abde333c1643241a1852fc142b8343d2b49da1f
z3y <91773180+zaki3mymy@users.noreply.github.com>
2026-05-24 18:44:13 +0900

    行番号が見づらかったので色を変えた (#69)

--- home/dot_config/nvim/lua/plugins/catppuccin.lua
+++ home/dot_config/nvim/lua/plugins/catppuccin.lua
@@ -1,12 +1,19 @@
 -- カラースキーマ(Catppuccin)
 return {
-  -- カラースキーマ(Catppuccin)
   "catppuccin/nvim",
   name = "catppuccin",
   priority = 1000,
   config = function()
     require("catppuccin").setup({
-      transparent_background = true
+      transparent_background = true,
+      highlight_overrides = {
+        all = function(palette)
+          return {
+            -- 行番号が見づらいので上書き
+            LineNr = { fg = palette.overlay0 },
+          }
+        end,
+      }
     })
   end,
 }
