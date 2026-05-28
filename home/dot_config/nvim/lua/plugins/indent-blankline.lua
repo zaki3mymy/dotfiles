51c1cb573110c3726be280037a5f851a470efae9
z3y <91773180+zaki3mymy@users.noreply.github.com>
2026-05-28 23:22:09 +0900

    背景透過に伴ったインデントの表示修正 (#71)
    - **背景透過に伴ってインデントの色が見づらくなったので修正**
    - **強調部分の縦線を太くした**

--- home/dot_config/nvim/lua/plugins/indent-blankline.lua
+++ home/dot_config/nvim/lua/plugins/indent-blankline.lua
@@ -19,19 +19,24 @@ return {
     -- create the highlight groups in the highlight setup hook, so they are reset
     -- every time the colorscheme changes
     hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-      vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#612f33" })
-      vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#615134" })
-      vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#284761" })
-      vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#61472f" })
-      vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#4a613c" })
-      vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#563461" })
-      vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#2b5a61" })
+      vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#8a4a52" })
+      vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#8a7448" })
+      vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#3f6b8f" })
+      vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#8a6848" })
+      vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#6b8a57" })
+      vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#7a4a8a" })
+      vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#3f7f8a" })
     end)
 
     require("ibl").setup({
       indent = {
         highlight = highlight,
         char = "▏",
+        tab_char = "▏",
+      },
+      scope = {
+        enabled = true,
+        char = "▎",
       },
     })
   end,
