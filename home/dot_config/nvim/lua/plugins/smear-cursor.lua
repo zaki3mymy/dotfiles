fe032b1d03e3c2cd2194f189a8f2625e976901ea
zaki3mymy <zaki.last@gmail.com>
2026-06-20 22:57:01 +0900

    nvimのカーソルアニメーションプラグインを追加

---
+++ home/dot_config/nvim/lua/plugins/smear-cursor.lua
@@ -0,0 +1,9 @@
+return {
+  "sphamba/smear-cursor.nvim",
+  opts = {
+    smear_between_buffers = false,
+    smear_between_neighbor_lines = false,
+    scroll_buffer_space = false,
+    smear_insert_mode = false,
+  },
+}
