541d11578dfd08e97a4d43a10104d8b58bba45ef
z3y <91773180+zaki3mymy@users.noreply.github.com>
2026-06-23 20:21:15 +0900

    LSPのリファレンスで開いたquickfixを自動で閉じる (#81)

--- home/dot_config/nvim/lua/config/filetype.lua
+++ home/dot_config/nvim/lua/config/filetype.lua
@@ -16,3 +16,11 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.opt.shiftwidth = 4
   end,
 })
+
+-- LSPのリファレンスで特定の項目を選択したらリファレンスのウィンドウ(quickfix)を自動で閉じる
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "qf",
+  callback = function()
+    vim.keymap.set("n", "<CR>", "<CR><Cmd>cclose<CR>", { noremap = false, silent = true })
+  end,
+})
