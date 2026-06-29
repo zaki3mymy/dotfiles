cf311944e59cc359b14c14085348eb2253916247
z3y <91773180+zaki3mymy@users.noreply.github.com>
2026-06-29 23:57:12 +0900

    ミニマップを表示 (#84)

--- home/dot_config/nvim/lua/plugins/mini.lua
+++ home/dot_config/nvim/lua/plugins/mini.lua
@@ -13,4 +13,38 @@ return {
       require("mini.surround").setup()
     end,
   },
+  {
+    "nvim-mini/mini.map",
+    version = false,
+    config = function()
+      local map = require("mini.map")
+      map.setup({
+        integrations = {
+          map.gen_integration.builtin_search(),
+          map.gen_integration.diff(),
+          map.gen_integration.diagnostic(),
+          map.gen_integration.gitsigns(),
+        },
+        symbols = {
+          scroll_line = "▶",
+        },
+      })
+      vim.keymap.set("n", "<leader>mmf", map.toggle_focus, { desc = "MiniMap.toggle_focus" })
+      vim.keymap.set("n", "<leader>mms", map.toggle_side, { desc = "MiniMap.toggle_side" })
+      vim.keymap.set("n", "<leader>mmt", map.toggle, { desc = "MiniMap.toggle" })
+
+      local ignore_ft = {
+        ["gitcommit"] = true,
+      }
+      vim.api.nvim_create_autocmd({ "VimEnter", "BufReadPost" }, {
+        callback = function()
+          local bt = vim.bo.buftype
+          local ft = vim.bo.filetype
+          if bt == "" and not ignore_ft[ft] then
+            map.open()
+          end
+        end,
+      })
+    end,
+  },
 }
